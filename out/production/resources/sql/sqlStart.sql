239d384542ab531c8ccbb69a3845d5ca28c8d6fd
KaFelLa92 <choidj10@gmail.com>
2026-01-10 16:22:13 +0900

    스프링로직초안완료

---
+++ out/production/resources/sql/sqlStart.sql
@@ -0,0 +1,12 @@
+# 시작할 때 켜고 시작
+
+-- 1. 올바른 DB 생성
+CREATE DATABASE IF NOT EXISTS seeyoulaterlamb
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- 2. 생성 확인
+SHOW DATABASES;
+
+-- 4. 사용할 DB 선택
+USE seeyoulaterlamb;
